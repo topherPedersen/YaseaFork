fa3c8e51278b22beddbd53e0e178166cc6074ac0
Christopher Pedersen <chrispedatx@gmail.com>
2017-12-17 01:43:34 -0600

    first modifications to yasea base

---
+++ app/src/main/java/net/ossrs/yasea/demo/OtherServiceActivity.java
@@ -0,0 +1,13 @@
+package net.ossrs.yasea.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OtherServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other_service);
+    }
+}
---
+++ app/src/main/java/net/ossrs/yasea/demo/YouTubeActivity.java
@@ -0,0 +1,13 @@
+package net.ossrs.yasea.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YouTubeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_you_tube);
+    }
+}
